ad3c441e7a7688f22dc3b31e1dcac73804cf9a43
Traveler 2077 <boris890310@gmail.com>
2025-08-11 18:04:43 +0800

    調整格式

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,10 @@
 <?php
 require_once __DIR__ . '/../db.php';           // 你原本的資料庫連線
 require_once __DIR__ . '/../includes/auth_guard.php';
-
 require_admin(); // ← 只有 admin 能進來
 ?>
+
+
 <!doctype html>
 <html lang="zh-Hant">
 <head>
